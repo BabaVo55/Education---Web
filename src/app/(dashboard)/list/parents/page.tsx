66f4c980f0fd85fa51d6b0b2f90087121207a543
BabaVo55 <ethemcode@outlook.com>
2025-04-15 13:16:03 +0100

    The Three Type Problem

---
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -0,0 +1,111 @@
+import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
+import TableSearch from "@/components/TableSearch";
+import { role, studentsData } from "@/lib/data";
+import Image from "next/image";
+import Link from "next/link";
+
+
+
+const columns = [
+    {
+        header:"Info",
+        accessor: "info",
+    },
+    {
+        header:"Student ID", 
+        accessor: "studentId", 
+        className: "hidden md:table-cell"
+    },
+    {
+        header:"Grade", 
+        accessor: "grade", 
+        className: "hidden md:table-cell"
+    },
+    {
+        header:"Phone",
+        accessor: "phone",
+        className: "hidden md:table-cell"
+    },
+    {
+        header:"Address", 
+        accessor: "address", 
+        className: "hidden md:table-cell"
+    },
+    {
+        header:"Actions", 
+        accessor: "actions", 
+        className: "hidden lg:table-cell"
+    }
+]
+
+type Parents = {
+    id:number;
+    studentId:string;
+    name:string;
+    email?:string;
+    photo:string;
+    phone?:string;
+    grade:string[];
+    class:string[];
+    address:string; 
+}
+
+export default function ParentsListPage(){
+
+    const renderRow = (item:Parents) => {
+        return (
+        <tr key={item.id} className="border-b border-gray-200 text-sm even:bg-slate-50 hover:odd:bg-ethemPurpleLight">
+            <td className="flex items-center gap-4 p-4">
+                <Image src={item.photo} alt="" width={40} height={40} className="md:hidden xl:block w-10 h-10 rounded-full object-cover" />
+                <div className="flex flex-col">
+                    <span  className="font-semibold">{item.name}</span>
+                    <p className="text-xs text-gray-500">{item.class}</p>
+                </div>
+            </td>
+            <td className="hidden md:table-cell">{item.studentId}</td>
+            <td className="hidden md:table-cell">{item.grade}</td>
+            <td className="hidden md:table-cell">{item.phone}</td>
+            <td className="hidden md:table-cell">{item.address}</td>
+            <td className="">
+                <div className="flex items-center gap-2">
+                    <Link href={`/list/teachers/${item.id}`}>
+                        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-ethemSky">
+                            <Image src="/view.png" alt="" width={16} height={16} />
+                        </button>
+                    </Link>
+                    {role == 'admin' && <button className="w-7 h-7 flex items-center justify-center rounded-full bg-ethemPurple">
+                        <Image src="/delete.png" alt="" width={16} height={16} />
+                    </button>}
+                </div>
+            </td>
+        </tr>
+    )}
+
+    return (
+        <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+             {/* Top section */}
+            <div className="flex justify-between"> 
+                <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
+                <div className="flex flex-col md:flex-row  items-center gap-4 w-full md:w-auto">
+                    <TableSearch />
+                    <div className="flex items-center gap-4 self-end">
+                        <button className="flex w-8 h-8 items-center justify-center rounded-full bg-ethemYellow">
+                            <Image src="/filter.png" alt="" width={14} height={14} />
+                        </button>
+                        <button className="flex w-8 h-8 items-center justify-center rounded-full bg-ethemYellow">
+                            <Image src="/sort.png" alt="" width={14} height={14} />
+                        </button>
+                       {role == 'admin' && <button className="flex w-8 h-8 items-center justify-center rounded-full bg-ethemYellow">
+                            <Image src="/plus.png" alt="" width={14} height={14} />
+                        </button>}
+                    </div>
+                </div>
+            </div>
+             {/* List section */}
+            <Table columns={columns} renderRow={renderRow} data={studentsData}/>
+             {/* Pagination section */}
+            <Pagination />
+        </div>
+    )
+}   
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,3 +1,83 @@
+    import React from "react";
+
+    // type Column = {
+    // header: string;
+    // accessor: string;
+    // className?: string;
+    // };
+
+    // type RenderRow = (item: any) => React.ReactNode;
+
+    // type Data = any[]
+
+    // type TableProps = {
+    // columns: Column[];
+    // renderRow: RenderRow;
+    // data: Data;
+    // };
+
+    // const Table = ({ columns, renderRow, data }: TableProps) => {
+    // return (
+    //     <table className="w-full mt-4 ml-4">
+    //     <thead>
+    //         <tr className="text-left text-gray-500 text-sm">
+    //         {columns.map((col) => (
+    //             <th className={col.className} key={col.accessor}>
+    //             {col.header}
+    //             </th>
+    //         ))}
+    //         </tr>
+    //     </thead>
+    //     <tbody>{data.map(item => renderRow(item))}</tbody>
+    //     </table>
+    // );
+    // };
+
+    // export default Table;
+
+
+// // ----------------------------------------------------------------------------
+
+
+// type Columns = {
+//     header:string;
+//     accessor:string;
+//     className?:string;
+// }[];
+
+// type RenderRow = (item:any) => React.ReactNode;
+
+// type Data = any[];
+
+
+// const Table = ({ columns, renderRow, data }: {
+//     columns: Columns;
+//     renderRow: RenderRow;
+//     data: Data;
+//   }) => {
+// return (
+//     <table className="w-full mt-4 ml-4">
+//         <thead>
+//             <tr className="text-left text-gray-500 text-sm">
+//                 {columns.map((col) => (
+//                     <th className={col.className} key={col.accessor}>
+//                         {col.header}
+//                     </th>
+//                 ))}
+//             </tr>
+//         </thead>
+//         <tbody>{data.map(item => renderRow(item))}</tbody>
+        
+//     </table>
+// )
+// }
+
+// export default Table;     
+
+
+// ----------------------------------------------------------------------------
+
+
 const Table = ({columns, renderRow, data}:{
     columns:{
         header:string;
@@ -24,4 +104,4 @@ const Table = ({columns, renderRow, data}:{
     )
 }
 
-export default Table;                  
+export default Table;  
